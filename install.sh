be892da84ba241416520e4f8edcfeb887d0e90f6
Daniel Proctor <dan@danielproctor.dev>
2023-04-02 18:06:17 +0900

    update to 38 - remove docker in favour of podman - bump version - remove sorting of packages to install

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ RELEASE="$(rpm -E %fedora)"
 
 INCLUDED_PACKAGES=($(jq -r "[(.all.include | (.all, select(.\"$SILVERBLUE_VERSION\" != null).\"$SILVERBLUE_VERSION\")[]), \
                              (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".include | (.all, select(.\"$SILVERBLUE_VERSION\" != null).\"$SILVERBLUE_VERSION\")[])] \
-                             | sort | unique[]" /tmp/packages.json))
+                             | unique[]" /tmp/packages.json))
 EXCLUDED_PACKAGES=($(jq -r "[(.all.exclude | (.all, select(.\"$SILVERBLUE_VERSION\" != null).\"$SILVERBLUE_VERSION\")[]), \
                              (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".exclude | (.all, select(.\"$SILVERBLUE_VERSION\" != null).\"$SILVERBLUE_VERSION\")[])] \
-                             | sort | unique[]" /tmp/packages.json))
+                             | unique[]" /tmp/packages.json))
 
 
 if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
