3e72c410ba3306df5363329ea22f7191b138c334
Daniel Proctor <dan@danielproctor.dev>
2023-04-06 23:21:17 +0900

    fix curl requests

--- install.sh
+++ install.sh
@@ -52,8 +52,8 @@ fi
 mkdir /usr/share/fonts/nerd-fonts
 curl -fLo "/usr/share/fonts/nerd-fonts/Caskaydia_Cove_Nerd_Font_Complete_Regular.otf" \
     https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/CascadiaCode/Regular/complete/Caskaydia%20Cove%20Nerd%20Font%20Complete%20Regular.otf
-curl -fLo "/usr/share/fonts/nerd-fonts/Caskaydia_Cove_Nerd_Font_Complete_Mono.otf" \
-    https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/CascadiaCode/Regular/complete/Caskaydia%20Cove%20Nerd%20Font%20Complete%20Mono.otf
+curl -fLo "/usr/share/fonts/nerd-fonts/Caskaydia_Cove_Nerd_Font_Complete_Mono_Regular.otf" \
+    https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/CascadiaCode/Regular/complete/Caskaydia%20Cove%20Nerd%20Font%20Complete%20Mono%20Regular.otf
 
 fc-cache -f /usr/share/fonts/nerd-fonts
 
