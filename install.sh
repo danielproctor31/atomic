f12edde2a033dbb07b4a5cdb8467f5575fecd34b
Daniel Proctor <dan@danielproctor.dev>
2023-04-06 23:07:16 +0900

    update install script

--- install.sh
+++ install.sh
@@ -49,12 +49,13 @@ else
 fi
 
 # install fonts
-curl -fLo "/usr/share/fonts/nerdfonts/Caskaydia Cove Nerd Font Complete Regular.otf" \
+mkdir /usr/share/fonts/nerd-fonts
+curl -fLo "/usr/share/fonts/nerd-fonts/Caskaydia_Cove_Nerd_Font_Complete_Regular.otf" \
     https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/CascadiaCode/Regular/complete/Caskaydia%20Cove%20Nerd%20Font%20Complete%20Regular.otf
-curl -fLo "/usr/share/fonts/nerdfonts/Caskaydia Cove Nerd Font Complete Mono.otf" \
+curl -fLo "/usr/share/fonts/nerd-fonts/Caskaydia_Cove_Nerd_Font_Complete_Mono.otf" \
     https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/CascadiaCode/Regular/complete/Caskaydia%20Cove%20Nerd%20Font%20Complete%20Mono.otf
 
-fc-cache -f /usr/share/fonts/nerdfonts
+fc-cache -f /usr/share/fonts/nerd-fonts
 
 # enable auto updates
 systemctl enable rpm-ostreed-automatic.timer
