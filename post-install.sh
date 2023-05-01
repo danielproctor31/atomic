ae6ec8e82797bfe51f1c4714a3b889d17fac3d32
Daniel <10681172+danielproctor31@users.noreply.github.com>
2023-05-01 11:00:21 +0100

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -4,9 +4,9 @@ set -ouex pipefail
 
 # install fonts
 mkdir /usr/share/fonts/nerd-fonts
-curl -fLo "/usr/share/fonts/nerd-fonts/Caskaydia_Cove_Nerd_Font_Complete_Regular.otf" \
+curl -fLo "/usr/share/fonts/nerd-fonts/CaskaydiaCoveNerdFont.ttf" \
     https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/CascadiaCode/Regular/CaskaydiaCoveNerdFont-Regular.ttf
-curl -fLo "/usr/share/fonts/nerd-fonts/Caskaydia_Cove_Nerd_Font_Complete_Mono_Regular.otf" \
+curl -fLo "/usr/share/fonts/nerd-fonts/CaskaydiaCoveNerdFontMono-Regular.ttf" \
     https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/CascadiaCode/Regular/CaskaydiaCoveNerdFontMono-Regular.ttf
 
 fc-cache -f /usr/share/fonts/nerd-fonts
