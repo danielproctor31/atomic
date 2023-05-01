dedf963505a5722c8b9f2d1e47d44c958966d8c3
Daniel <10681172+danielproctor31@users.noreply.github.com>
2023-05-01 13:34:46 +0100

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -9,8 +9,6 @@ curl -fLo "/usr/share/fonts/nerd-fonts/CaskaydiaCoveNerdFont.ttf" \
 curl -fLo "/usr/share/fonts/nerd-fonts/CaskaydiaCoveNerdFontMono-Regular.ttf" \
     https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/CascadiaCode/Regular/CaskaydiaCoveNerdFontMono-Regular.ttf
 
-fc-cache -f /usr/share/fonts/nerd-fonts
-
 # enable auto updates
 systemctl enable rpm-ostreed-automatic.timer
 systemctl enable flatpak-system-update.timer
