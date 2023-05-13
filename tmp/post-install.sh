7f540b964fa807b9eea2bea48e3ff16c7b6b0c7c
Daniel Proctor <dan@danielproctor.dev>
2023-05-13 21:27:40 +0100

    - move dconf service - remove gnome items from kinoite image

--- tmp/post-install.sh
+++ tmp/post-install.sh
@@ -8,6 +8,13 @@ systemctl enable flatpak-system-update.timer
 systemctl --global enable flatpak-user-update.timer
 systemctl --global enable distrobox-update.timer
 
-# enable dconf update
-systemctl unmask dconf-update.service
-systemctl enable dconf-update.service
+if [ $SILVERBLUE_VERSION = "silverblue" ]; then
+  # enable dconf update
+  systemctl unmask dconf-update.service
+  systemctl enable dconf-update.service
+else
+  # remove gnome specific items
+  rm /etc/systemd/system/dconf-update.service
+  rm -r /usr/share/fonts/Ubuntu
+  rm -r /usr/share/fonts/Ubuntu-Mono
+fi
